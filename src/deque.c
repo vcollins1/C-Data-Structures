e3068d5648af5aa4c9ce4087e60a9a822ecb7f73
Vontrelle Collins <vontrellecollins@gmail.com>
2026-05-09 15:43:27 -0400

    Implement deque function and add the deque to the cmake library.

--- src/deque.c
+++ src/deque.c
@@ -2,41 +2,41 @@
 #include "list.h"
 
 ds_deque_t* deque_create(size_t data_size) {
-    return NULL;
+    return list_create(data_size);
 }
 
 void deque_destroy(ds_deque_t* deque, clear_callback func) {
-
+    list_destroy(deque, func);
 }
 
 size_t deque_size(ds_deque_t *deque) {
-    return 0;
+    return list_size(deque);
 }
 
 bool deque_empty(ds_deque_t* deque) {
-    return false;
+    return list_empty(deque);
 }
 
 ds_status_code_t deque_front(ds_deque_t* deque, void* output) {
-    return DS_SUCCESS_OK;
+    return list_front(deque, output);
 }
 
 ds_status_code_t deque_back(ds_deque_t* deque, void* output) {
-    return DS_SUCCESS_OK;
+    return list_back(deque, output);
 }
 
 ds_status_code_t deque_push_front(ds_deque_t* deque, void* data) {
-    return DS_SUCCESS_OK;
+    return list_unshift(deque, data);
 }
 
 ds_status_code_t deque_push_back(ds_deque_t* deque, void* data) {
-    return DS_SUCCESS_OK;
+    return list_add_back(deque, data);
 }
 
 ds_status_code_t deque_pop_front(ds_deque_t* deque, clear_callback func) {
-    return DS_SUCCESS_OK;
+    return list_shift(deque, func);
 }
 
 ds_status_code_t deque_pop_back(ds_deque_t*  deque, clear_callback func) {
-    return DS_SUCCESS_OK;
+    return list_remove_back(deque, func);
 }
